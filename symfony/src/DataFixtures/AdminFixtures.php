54e4be63f9ee4a38aafb9bd9f4afc4423206b5f1
pavellisovskij <pavel.lisovskij93@gmail.com>
2020-12-13 13:14:12 +0300

    Initial commit

---
+++ symfony/src/DataFixtures/AdminFixtures.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Admin;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class AdminFixtures extends Fixture
+{
+    private $encoder;
+
+    private $em;
+
+    public function __construct(UserPasswordEncoderInterface $encoder, EntityManagerInterface $entityManager)
+    {
+        $this->encoder = $encoder;
+        $this->em = $entityManager;
+    }
+
+    public function load(ObjectManager $manager)
+    {
+        $adminsData = [
+            0 => [
+                'username' => 'admin',
+                'role' => ['ROLE_ADMIN'],
+                'password' => 123654
+            ]
+        ];
+
+        foreach ($adminsData as $admin) {
+            $newAdmin = new Admin();
+            $newAdmin->setUsername($admin['username']);
+            $newAdmin->setPassword($this->encoder->encodePassword($newAdmin, $admin['password']));
+            $newAdmin->setRoles($admin['role']);
+            $this->em->persist($newAdmin);
+        }
+
+        $this->em->flush();
+    }
+}
---
+++ symfony/src/DataFixtures/UserFixtures.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class UserFixtures extends Fixture
+{
+    private $encoder;
+
+    private $em;
+
+    public function __construct(UserPasswordEncoderInterface $encoder, EntityManagerInterface $entityManager)
+    {
+        $this->encoder = $encoder;
+        $this->em = $entityManager;
+    }
+
+    public function load(ObjectManager $manager)
+    {
+        $usersData = [
+            0 => [
+                'email' => 'user@example.com',
+                'role' => ['ROLE_USER'],
+                'password' => 123654
+            ]
+        ];
+
+        foreach ($usersData as $user) {
+            $newUser = new User();
+            $newUser->setEmail($user['email']);
+            $newUser->setPassword($this->encoder->encodePassword($newUser, $user['password']));
+            $newUser->setRoles($user['role']);
+            $this->em->persist($newUser);
+        }
+
+        $this->em->flush();
+    }
+}
