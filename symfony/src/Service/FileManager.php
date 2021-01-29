d35f1f9f973880296111d5c8faf49ec782bb459d
pavellisovskij <pavel.lisovskij93@gmail.com>
2021-01-29 22:03:33 +0300

    Added FileManager service

---
+++ symfony/src/Service/FileManager.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Service;
+
+use Symfony\Component\Filesystem\Exception\ExceptionInterface;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+
+class FileManager
+{
+    private $uploadsDir;
+    private $fs;
+
+    public function __construct(string $uploadsDir)
+    {
+        $this->uploadsDir = $uploadsDir;
+        $this->fs = new Filesystem();
+    }
+
+    /**
+     * @param UploadedFile $file
+     * @param string|null $path
+     * @return string
+     */
+    public function upload(UploadedFile $file, string $path = null): string
+    {
+        $fileName = md5(uniqid()).'.'.$file->guessExtension();
+
+        if ($path !== null) {
+            $path = $this->transformPath($path);
+            $this->uploadsDir = $this->uploadsDir . $path;
+        }
+        
+        try {
+            $file->move($this->uploadsDir, $fileName);
+        } catch (FileException $e) {
+            dd($e->getMessage());
+        }
+        
+        return $path . $fileName;
+    }
+
+    /**
+     * @param string $filePath
+     * @return bool
+     */
+    public function delete(string $filePath): bool
+    {
+        try {
+            $this->fs->remove($this->uploadsDir . '/' . $filePath);
+            return true;
+        } catch (ExceptionInterface $e) {
+            dd($e->getMessage());
+        }
+    }
+
+    private function transformPath(string $path): string
+    {
+        if (mb_substr($path, 0, 1) !== '/') $path = '/' . $path;
+        if (mb_substr($path, -1) !== '/') $path .= '/';
+
+        return $path;
+    }
+}
